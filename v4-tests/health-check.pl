4c84db67dd11ef92715da64a9c1567b3c935a030
brian <bmulhall@idscan.net>
2023-04-02 22:10:06 -0400

    verify endpoint test case is working now using the Mime::Base64 perl module to encode the images with

--- v4-tests/health-check.pl
+++ v4-tests/health-check.pl
@@ -3,51 +3,100 @@ use v5.20;
 use warnings;
 use feature qw(signatures);
 no warnings qw(experimental::signatures);
-use Data::Dumper;
-use JSON;
-use LWP::UserAgent;
+use Mojo::Base -strict;
+use Mojo::UserAgent;
+use Mojo::Util qw(dumper);
+use JSON::Validator;
+
 use Test::Simple tests => 2;
 
 my $secret_key = 'sk_edbe2987-eb2c-4c20-843b-e663d583c7a3';
 
-my $health_check_url = 'https://stage.dvs2.idware.net/api/hc';
-my $liveness_url = 'https://stage.dvs2.idware.net/api/liveness';
+
+TESTCASEONE: {
+my $jv = JSON::Validator->new;
+$jv = $jv->schema('file:///home/brian/source/repos/dvs-test-app/v4-tests/json-schemas/health-check-schema.json');
 
 
+my $staging_health_check_url = 'https://stage.dvs2.idware.net/api/hc';
 
-my $ua = LWP::UserAgent->new;
+my $ua = Mojo::UserAgent->new;
 
-# set custom HTTP request header fields
-my $req = HTTP::Request->new(GET => $health_check_url);
-$req->header('content-type' => '*/*');
-$req->header('Authorization' => "Bearer $secret_key");
+my $tx = $ua->get(
+    $staging_health_check_url => {
+        'content-type'  => '*/*',
+        'Authorization' => "Bearer $secret_key"
+    }
+);
 
-my $resp = $ua->request($req);
-if ($resp->is_success) {
-    my $message = $resp->decoded_content;
-    my $text = JSON::decode_json($message);
-    print Dumper($text->{'status'});
-    ok($text->{"status"} =~ m/Healthy/);
-    
+my $res = $tx->result;
+say do {
+    if ( $res->is_success ) {
+        # 2xx series
+        'Health Check Request worked!';
+    }
+    elsif ( $res->is_redirect ) {
+        # 3xx series
+        'Need to look over there: ' . $res->headers->location;
+    }
+    elsif ( $res->is_client_error ) {    # 4xx series
+        'Problem with the request';
+    }
+    elsif ( $res->is_server_error ) {    # 5xx series
+        'Problem with the response (server fault)';
+    }
+};
+
+if ( $res->is_success ) {
+    my @errors = $jv->validate($res->json);
+    print (@errors);
+    ok( $res->json->{"status"} =~ m/Healthy/ );
+
 }
 else {
-    print "HTTP GET error code: ", $resp->code, "\n";
-    print "HTTP GET error message: ", $resp->message, "\n";
+    print "HTTP GET error code: ",    $res->code,    "\n";
+    print "HTTP GET error message: ", $res->message, "\n";
 }
 
+}
 
+TESTCASETWO:{
+my $staging_liveness_url     = 'https://stage.dvs2.idware.net/api/liveness';
 
-# set custom HTTP request header fields
-$req = HTTP::Request->new(GET => $liveness_url);
-$req->header('content-type' => '*/*');
-$req->header('Authorization' => "Bearer $secret_key");
+my $ua = Mojo::UserAgent->new;
 
-$resp = $ua->request($req);
-if ($resp->is_success) {
-    my $message = $resp->decoded_content;
-    ok($message =~ m/Healthy/);
+my $tx = $ua->get(
+    $staging_liveness_url => {
+        'content-type'  => '*/*',
+        'Authorization' => "Bearer $secret_key"
+    }
+);
+
+my $res = $tx->result;
+say do {
+    if ( $res->is_success ) {
+
+        # 2xx series
+        'Liveness Request worked!';
+    }
+    elsif ( $res->is_redirect ) {
+
+        # 3xx series
+        'Need to look over there: ' . $res->headers->location;
+    }
+    elsif ( $res->is_client_error ) {    # 4xx series
+        'Problem with the request';
+    }
+    elsif ( $res->is_server_error ) {    # 5xx series
+        'Problem with the response (server fault)';
+    }
+};
+
+if ( $res->is_success ) {
+    ok( $res->body =~ m/Healthy/ );
 }
 else {
-    print "HTTP GET error code: ", $resp->code, "\n";
-    print "HTTP GET error message: ", $resp->message, "\n";
+    print "HTTP GET error code: ",    $res->code,    "\n";
+}
+
 }
--- v4-tests/verify.pl
+++ v4-tests/verify.pl
@@ -5,61 +5,131 @@ use LWP::UserAgent;
 use Data::Dumper;
 use MIME::Base64;
 use File::Slurp;
+use Mojo::Base -strict;
+use Mojo::JSON;
+use Mojo::UserAgent;
+use Mojo::Util qw(dumper);
 
 
-my $filename = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\test-data\Base64Images.txt';
+# my $filename = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\test-data\Base64Images.txt';
 
-my $front_image_filepath = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\test-data\front.jpg';
-my $back_image_filepath = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\test-data\back.jpg';
-my $face_image_filepath = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\test-data\face.jpg';
+my $front_image_filepath = '/home/brian/source/repos/dvs-test-app/test-data/07-05-2022_7ec25bf0-e9fd-4774-addd-d395b3aad0b1_FrontImage.jpg';
+my $back_image_filepath = '/home/brian/source/repos/dvs-test-app/test-data/07-05-2022_7ec25bf0-e9fd-4774-addd-d395b3aad0b1_BackOrSecondImage.jpg';
+my $face_image_filepath = '/home/brian/source/repos/dvs-test-app/test-data/07-05-2022_7ec25bf0-e9fd-4774-addd-d395b3aad0b1_FrontImage.jpg';
 
-open my $front_image_handle, '<', $front_image_filepath;
+open my $front_image_handle, '<', $front_image_filepath or die $!;
 my $front_image = <$front_image_handle>;
 
-open my $back_image_handle, '<', $back_image_filepath;
+open my $back_image_handle, '<', $back_image_filepath or die $!;
 my $back_image = <$back_image_handle>;
 
-open my $face_image_handle, '<', $face_image_filepath;
-my $face_image = <$face_image_handle>;
+# open my $face_image_handle, '<', $face_image_filepath;
+# my $face_image = <$face_image_handle>;
 
 close $front_image_handle;
 close $back_image_handle;
-close $face_image_handle;
+# close $face_image_handle;
 
-my $ua = LWP::UserAgent->new;
-
-
-#print $front_image;
+# my $ua = LWP::UserAgent->new;
 
 
-my $v4_verify_url = 'https://stage.dvs2.idware.net/api/v4/Verify'; 
-
-#my $verify_path = 'Verify';
+# #print $front_image;
 
 
-my $input_filename = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\input.txt';
+# my $v4_verify_url = 'https://stage.dvs2.idware.net/api/v4/Verify'; 
 
-my $json = read_file($input_filename);
+# #my $verify_path = 'Verify';
 
-my $output_filename = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\output.txt';
+
+# my $input_filename = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\input.txt';
+
+# my $json = read_file($input_filename);
+
+# my $output_filename = 'C:\Users\brian\source\repos\perl-projects\dvs-test-app\output.txt';
+# open(my $fh, '>', $output_filename) or die "Could not open file '$output_filename' $!";
+
+
+# my $req = HTTP::Request->new( 'POST', $v4_verify_url );
+# $req->header( 'Content-Type' => 'application/json' );
+# $req->header( 'Authorization' => 'Bearer sk_edbe2987-eb2c-4c20-843b-e663d583c7a3' );
+# $req->content( $json );
+
+# my $resp = $ua->request($req);
+# if ($resp->is_success) {
+#     my $message = $resp->decoded_content;
+#     print $fh Dumper($message);
+      
+# }
+# else {
+#     print "HTTP POST error code: ", $resp->code, "\n";
+#     print $fh Dumper($resp);
+        
+# }
+
+# close $fh;
+
+
+my $encoded_front_image = MIME::Base64::encode_base64($front_image,'');
+my $encoded_back_image = MIME::Base64::encode_base64($back_image,'');
+
+
+use Mojo::UserAgent;
+my $ua = Mojo::UserAgent->new;
+
+my $output_filename = 'output.txt';
 open(my $fh, '>', $output_filename) or die "Could not open file '$output_filename' $!";
 
+open my $handle, '<', '/home/brian/source/repos/dvs-test-app/test-data/Base64Images.txt';
+chomp(my @lines = <$handle>);
+close $handle;
 
-my $req = HTTP::Request->new( 'POST', $v4_verify_url );
-$req->header( 'Content-Type' => 'application/json' );
-$req->header( 'Authorization' => 'Bearer sk_edbe2987-eb2c-4c20-843b-e663d583c7a3' );
-$req->content( $json );
+my %body = (
+    frontImageBase64        =>  $encoded_front_image,
+    backOrSecondImageBase64 => $encoded_back_image,
+    faceImageBase64         => undef,
+    ssn => '5308',
+    trackString => {
+        trackString   => 'QAoeDUFOU0kgNjM2MDAzMDgwMDAyREwwMDQxMDI1NVpNMDI5NjAwMDhETERBUUctNDIwLTEzNS00MjktMjk4CkRDU0dMT1dBQ0sKRERFTgpEQUNEQU5JRUwKRERGTgpEQURKT0hOCkRER04KRENBQ00KRENCTk9ORQpEQ0ROT05FCkRCRDA0MTUyMDE3CkRCQjA0MTcxOTkxCkRCQTA0MTcyMDI0CkRCQzEKREFVMDczIGluCkRBWVVOSwpEQUcxMDUgT0FLSFVSU1QgQVZFCkRBSUVTU0VYCkRBSk1ECkRBSzIxMjIxMDAwMCAgCkRDRjI1MDBGQUQ5OQpEQ0dVU0EKRENLMTAwMDg5MzEwMApEREFGCkREQjA2MjAyMDE2CkRBVzE5Nw1aTVpNQTAxDQ==',
+        barcodeParams => undef
+    },
+    documentType => 1,
+    overriddenSettings => {
+        isOCREnabled => Mojo::JSON->false,
+        isAddressCheckEnabled => Mojo::JSON->true,
+        isDMVEnabled => Mojo::JSON->false,
+        isDMVPartialEnabled => Mojo::JSON->false,
+        isIdentiFraudEnabled => Mojo::JSON->false,
+        isIdentiFraudFullCaseEnabled => Mojo::JSON->false,
+        isDmvOrIdentiFraudEnabled => Mojo::JSON->false,
+        isDmvAndIdentiFraudEnabled => Mojo::JSON->false,
+        isDmvOrIdentiFraudPartialEnabled => Mojo::JSON->false,
+        isDmvAndIdentiFraudPartialEnabled => Mojo::JSON->false,
+        isFaceRequiredOnDocument => Mojo::JSON->true,
+        isGenderCheckEnabled => Mojo::JSON->true,
+        isAgeCheckEnabled => Mojo::JSON->true,
+        isShuftiEnabled => Mojo::JSON->true,
+        isBackOrSecondImageProcessing => Mojo::JSON->true,
+        isFaceMatchEnabled => Mojo::JSON->false
+    },
+    metadata => {
+        captureMethod => '111',
+        userAgent => '',
+        frontEndMetadata => '',
+        otherMetadata => ''
+  }
+);
 
-my $resp = $ua->request($req);
-if ($resp->is_success) {
-    my $message = $resp->decoded_content;
-    print $fh Dumper($message);
-      
-}
-else {
-    print "HTTP POST error code: ", $resp->code, "\n";
-    print $fh Dumper($resp);
-        
-}
+
+
+
+my $staging_verfy_url = 'https://stage.dvs2.idware.net/api/v4/Verify';
+
+my $tx = $ua->post($staging_verfy_url => {Accept => 'application/json', Authorization => 'Bearer sk_edbe2987-eb2c-4c20-843b-e663d583c7a3'} => json => \%body );
+print $fh dumper $tx->req;
+
+my $res = $tx->result->content;
+
+print dumper $res;
 
 close $fh;
+
